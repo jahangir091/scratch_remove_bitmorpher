9c7f3c3b5acdedf06940c3753758522f1750e634
jahangir <jahangir.cse09@gmail.com>
2024-03-25 17:15:14 +0600

    updated api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import torch
 
 from fastapi import FastAPI, Body
 import uvicorn
@@ -26,18 +27,25 @@ async def generate_mask_image(
 ):
     start_time = time.time()
 
-    download_scratch_remover_model()
-    pil_image = decode_base64_to_image(image).convert("RGB")
-    out_image = remove_scratch_using_mask(pil_image, upscale)
-
     out_images_directory_name = '/scratch_images/'
-
     out_image_path = get_img_path(out_images_directory_name)
-    out_image.save(out_image_path)
+
+    try:
+        download_scratch_remover_model()
+        pil_image = decode_base64_to_image(image).convert("RGB")
+        out_image = remove_scratch_using_mask(pil_image, upscale)
+        out_image.save(out_image_path)
+        success = True
+        message = "Returned output successfully"
+    except Exception as e:
+        success = False
+        message = str(e)
+    finally:
+        torch.cuda.empty_cache()
 
     return {
-        "success": True,
-        "message": "Returned output successfully",
+        "success": success,
+        "message": message,
         "server_process_time": time.time() - start_time,
         "output_image_url": '/media' + out_images_directory_name + out_image_path.split('/')[-1]
     }
